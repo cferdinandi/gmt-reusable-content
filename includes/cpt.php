d8262537c066447a9c2378c69747208e193c4ed1
cferdinandi <chris@gomakethings.com>
2016-12-02 11:29:50 -0500

    v1.0.4 Restored visibility in Dashboard

--- includes/cpt.php
+++ includes/cpt.php
@@ -24,6 +24,7 @@
 			'labels'        => $labels,
 			'description'   => 'Holds our snippets and snippet-specific data',
 			// 'public'        => true,
+			'show_ui'       => true,
 			// 'menu_position' => 5,
 			'menu_icon'     => 'dashicons-editor-paste-text',
 			'hierarchical'  => false,
